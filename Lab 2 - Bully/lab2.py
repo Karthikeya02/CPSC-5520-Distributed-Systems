bf00caf4d48ffbce531ebf2bf7a206f1d6502e44
Karthikeya02 <karthikeya.panangipalli.02@gmail.com>
2024-11-10 10:07:50 -0800

    Add File Headers

--- Lab 2 - Bully/lab2.py
+++ Lab 2 - Bully/lab2.py
@@ -1,3 +1,11 @@
+'''
+CPSC 5520, Seattle University
+This is free and unencumbered software released into the public domain.
+:Authors: Karthikeya Panangipalli
+:Version: f24
+Bully Algorithm Implementation
+'''
+
 import sys
 import socket
 import pickle
